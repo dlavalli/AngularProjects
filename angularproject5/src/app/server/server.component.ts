34ec799d85849d6eb1ac9509bc5f993b29193de5
Daniel Lavalliere <d.lavalliere@videotron.ca>
2021-03-30 22:49:26 -0400

    Updates with databinding code

--- angularproject5/src/app/server/server.component.ts
+++ angularproject5/src/app/server/server.component.ts
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+  // This version hardcode the variables to display in the html page
+  //serverId : number = 10;
+  //serverStatus : string = 'offline';
+  serverId = 10;
+  serverStatus = 'offline';
 
+  // This is a defintion of a method (notice NO function keyword for an objct method)
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
 
--- angularproject5/src/app/servers/servers.component.ts
+++ angularproject5/src/app/servers/servers.component.ts
@@ -12,10 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = `Server was created! Server Name is ${this.serverName}`;
+  }
+  onUpdateServerName(event: Event) {
+    // event.data is the text as it is typed
+    // event is the entire event object
+
+    // If passing type event : any
+    // console.log(event.target.value);
+
+    // If passing type event : Event (need type cast)
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }

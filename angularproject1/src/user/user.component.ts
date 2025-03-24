905bb37c0a7dcf2e3a392429fc05cbedcdc479e1
Daniel Lavalliere <d.lavalliere@videotron.ca>
2025-03-24 17:55:44 -0400

    Added @Input support for component parameters

--- angularproject1/src/user/user.component.ts
+++ angularproject1/src/user/user.component.ts
@@ -1,6 +1,4 @@
-import { Component, computed, input } from '@angular/core';
-// import { Component, Input, input } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-user',
   imports: [],
@@ -8,26 +6,13 @@ import { Component, computed, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // without using input() function (ie signal)
-  // @Input({required: true}) avatar !: String;
-  // @Input({required: true}) name !: String;
 
-  // get imagePath() {
-  //  return 'assets/users/' + this.avatar;
-  // }
+  @Input({required: true}) avatar !: String;
+  @Input({required: true}) name !: String;
 
-
-  // Using the (internally) RO input ie: avatar used as a signal
-  avatar = input<string>('');
-
-  // Using the (internally )RO input ie: name used as a signal
-  // Can alternatively indicate it is required
-  // name = input<string>('');
-  name = input.required<string>();
-
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  });
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {}
 }
